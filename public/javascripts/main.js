519d38ec8984aa03372102163bb4c44e15224e5b
whothat <benhuff322@gmail.com>
2016-07-25 12:18:55 -0700

    begin making project page dynamic

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,35 @@
 var csApp = angular.module( 'csApp', [] );
 
+var projects = [
+{
+  title: 'Micromouse',
+  description: ' is the best project!',
+  links:
+  {
+    GitHub: 'https://github.com/SRJC-Computer-Science-Club/micromouse',
+    waffleIO: 'https://waffle.io/SRJC-Computer-Science-Club/micromouse'
+  },
+  status: 'in progress',
+  members: ['Benjamin Hough','Joshua Murphy','Erick Sanchez','Seaney Shell','Oran Collins']
+},
+{
+  title: 'Function Fighters',
+  description: ' is the best project!'
+},
+{
+  title: 'CS Website',
+  description: ' is the best project!'
+} ];
+
+
+
+
 csApp.controller( 'ProjectListController', function ProjectListController( $scope )
 {
-	$scope.projects = [
-	{
-		name: 'Micromouse',
-		description: ' is the best project!'
-	},
-	{
-		name: 'Function Fighters',
-		description: ' is the best project!'
-	},
-	{
-		name: 'CS Website',
-		description: ' is the best project!'
-	} ]
-});
+	$scope.projects = projects;
+} );
+
+csApp.controller( 'ProjectPageController', function ProjectListController( $scope )
+{
+	$scope.project = projects[0];
+} );
