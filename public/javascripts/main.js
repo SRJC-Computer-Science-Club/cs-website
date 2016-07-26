e679067de28249334a5b75b43199e2a1f236dae3
whothat <benhuff322@gmail.com>
2016-07-25 17:19:22 -0700

    continue work on project and home page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,8 +6,9 @@ var projects = [
   description: ' is the best project!',
   links:
   {
-    GitHub: 'https://github.com/SRJC-Computer-Science-Club/micromouse',
-    waffleIO: 'https://waffle.io/SRJC-Computer-Science-Club/micromouse'
+    checkvist: 'https://checkvist.com/checklists/542765',
+    waffle: 'https://waffle.io/SRJC-Computer-Science-Club/micromouse',
+    github: 'https://github.com/SRJC-Computer-Science-Club/micromouse'
   },
   status: 'in progress',
   members: ['Benjamin Hough','Joshua Murphy','Erick Sanchez','Seaney Shell','Oran Collins']
@@ -22,6 +23,31 @@ var projects = [
 } ];
 
 
+var services = {
+  github:
+  {
+    name: 'GitHub',
+    icon_sm: 'github-icon-sm.png',
+    icon_md: '',
+    icon_lg: ''
+  },
+  waffle:
+  {
+    name: 'WaffleIO',
+    icon_sm: 'waffleio-icon-sm.png',
+    icon_md: '',
+    icon_lg: ''
+  },
+  checkvist:
+  {
+    name: 'Checkvist',
+    icon_sm: 'checkvist-icon-sm.png',
+    icon_md: '',
+    icon_lg: ''
+  }
+};
+
+
 
 
 csApp.controller( 'ProjectListController', function ProjectListController( $scope )
@@ -32,4 +58,5 @@ csApp.controller( 'ProjectListController', function ProjectListController( $scop
 csApp.controller( 'ProjectPageController', function ProjectListController( $scope )
 {
 	$scope.project = projects[0];
+  $scope.services = services;
 } );
