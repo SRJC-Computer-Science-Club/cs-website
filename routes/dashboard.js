8f24120e8a2bf60a792a925e1593b9ffc4ca2a68
Erick Sanchez <esericksanc@gmail.com>
2017-03-25 22:20:54 -0700

    Dashboard
    List all projects and events, for now

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,11 +5,29 @@ var helper = require('./helper_methods');
 
 router.get('/', function(req, res, next) {
   var navbar = {
-    active: '',
+    active: 'dashboard',
     links: []
   };
 
-	res.render('dashboard', {dashboard: true, title: 'CS Dashboard', navbar: navbar});
+	var projects = tempDB.projects;
+
+	for ( var project of projects) {
+		project.members = helper.findProjectMembers( project)
+	}
+
+	var login = null;
+
+	login = {
+		id: 1,
+		token: "123456789",
+		first_name: "Erick",
+		last_name: "Sanchez",
+		role: 0
+	}
+
+	login.name = login.first_name + ' ' + login.last_name;
+
+	res.render('dashboard', {dashboard: true, title: 'CS Dashboard', token: login, navbar: navbar, my_projects: projects, my_events: helper.findAllEvents().upcoming_events, helper: helper});
 })
 
 module.exports = router;
